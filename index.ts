53016f5c3e3bf9a99e2ad0a4c1272c686abb29d6
Nik Coughlin <nrkn.com@gmail.com>
2018-02-28 18:31:56 +1300

    Change types oops

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-export interface Rgba {
+export interface IRgba {
   r: number
   g: number
   b: number
   a: number
 }
 
-export const Rgba = ( r = 0, g = 0, b = 0, a = 0 ) : Rgba => {
+export const Rgba = ( r = 0, g = 0, b = 0, a = 0 ) : IRgba => {
   r = Math.floor( r < 0 ? 0 : r > 255 ? 255 : r )
   g = Math.floor( r < 0 ? 0 : r > 255 ? 255 : r )
   b = Math.floor( r < 0 ? 0 : r > 255 ? 255 : r )
